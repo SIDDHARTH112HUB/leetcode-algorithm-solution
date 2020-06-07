0f4d4a88535f0f8bb631adc074a4c55c6f256fe3
jianchaoche <chejianchao2012@gmail.com>
2020-06-07 18:04:05 -0400

    865 solution

--- 0001-0999/0865_smallest-subtree-with-all-the-deepest-nodes.cpp
+++ 0001-0999/0865_smallest-subtree-with-all-the-deepest-nodes.cpp
@@ -5,62 +5,28 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    map<TreeNode*, TreeNode*> parent;
-    int findParent(TreeNode* root){
-        if( !root ) return 0;
-        int l = 1, r= 1;
-        if( root->left){
-            parent[root->left ] = root;
-            
-            l = 1+findParent(root->left);
-        }
-        if( root->right){
-            parent[root->right] = root;
-            r = 1+findParent(root->right);
-        }
-        return max(l, r);
+    TreeNode* ans = NULL;
+    int depth = 0;
+    TreeNode* subtreeWithAllDeepest(TreeNode* root) {
+        dfs(root, 1);
+        return ans;
     }
     
-    void findDeepest(TreeNode* root, int h, set<TreeNode*> &dict ){
-        if(h == 1){
-            if( root->left || root->right )
-                cout<<"not the deepest "<<endl;
-            dict.insert(root);
-            return;
+    int dfs(TreeNode* root, int d) {
+        if(!root) return d;
+        int l = dfs(root->left, d + 1);
+        int r = dfs(root->right, d + 1);
+        if(l >= depth && r >= depth) {
+            depth = max(l, r);
+            ans = root;
         }
-        if( root->left ){
-            findDeepest(root->left, h-1, dict);
-        }
-        if( root->right ){
-            findDeepest(root->right, h-1, dict);
-        }
-        
-    }
-    TreeNode* subtreeWithAllDeepest(TreeNode* root) {
-        if( !root ) return NULL;
-        int h = findParent(root);
-        set<TreeNode*> node;
-        //cout<<"height "<<h<<endl;
-        if( h == 1 )
-            return root;
-        findDeepest(root, h, node );
-        if( node.size() == 1 )
-            return *node.begin();
-        while(node.size()){
-            set<TreeNode*> tmp;
-            for( auto n:node){
-                tmp.insert(parent[n]);
-            }
-            if( tmp.size() == 1 )
-                return *tmp.begin();
-            else
-                node = tmp;
-        }
-        return NULL;
+        return max(l, r);
     }
 };
