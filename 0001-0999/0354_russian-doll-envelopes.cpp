974b5fef16df0b868028dd13549074e7cc282142
jianchaoche <chejianchao2012@gmail.com>
2020-06-10 02:09:56 -0400

    354 solution

--- 0001-0999/0354_russian-doll-envelopes.cpp
+++ 0001-0999/0354_russian-doll-envelopes.cpp
@@ -1,20 +1,23 @@
 //Link: https://leetcode.com/problems/russian-doll-envelopes/ 
 class Solution {
 public:
-    int maxEnvelopes(vector<pair<int, int>>& envelopes) {
+    int maxEnvelopes(vector<vector<int> >& envelopes) {
         if( envelopes.size() == 0 ) return 0;
-        sort(envelopes.begin(), envelopes.end());
-        int dp[envelopes.size()] = {0};
-        dp[0] = 1;
+        sort(envelopes.begin(), envelopes.end(), [](auto&a, auto &b){
+            if(a[0] == b[0]) return a[1] > b[1];
+            return a[0] < b[0];
+        });
+        vector<int> dp;
+        
         int ans = 1;
-        for( int i = 1; i< envelopes.size(); i++){
-            dp[i] = 1;
-            for( int j = 0; j<i; j++){
-                if( envelopes[i].first > envelopes[j].first && envelopes[i].second > envelopes[j].second)
-                    dp[i] = max(dp[i], dp[j]+1);
+        for( int i = 0; i< envelopes.size(); i++){
+            auto it = lower_bound(dp.begin(), dp.end(), envelopes[i][1]);
+            if(it == dp.end()) {
+                dp.push_back(envelopes[i][1]);
+            }else {
+                *it = envelopes[i][1];
             }
-            ans = max(ans, dp[i]);
         }
-        return ans;
+        return dp.size();
     }
 };
